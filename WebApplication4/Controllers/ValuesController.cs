1b55b698de26dfc4da4b88b6385389699faad6e8
RupinderArticulate <rupinder@articulatesolution.com>
2021-02-10 13:21:49 +0530

    Add project files.

---
+++ WebApplication4/Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+        [HttpPost]
+        public DataTable getdata()
+        {
+            SqlConnection co = new SqlConnection("Data Source=111.11.11.11;Initial Catalog=database;Integrated Security=False;User ID=username;password=Asd@123;Connect Timeout=15;Encrypt=False;Packet Size=4096");
+            SqlCommand cmd = new SqlCommand("select * from identityuser",co);
+            SqlDataAdapter ad = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            ad.Fill(dt);
+            return dt;
+
+        }
+    }
+}
---
+++ WebApplication4/Models/StatusInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class StatusInfo
+    {
+        public string message { get; set; }
+        public string data { get; set; }
+    }
+    //public class 
+}
